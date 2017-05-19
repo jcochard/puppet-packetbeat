9ae320e47656040b78426d562d9a3328ef1f9ba1
Jason Cochard <jasonc@uber.com>
2017-05-19 15:58:53 -0700

    use the rpm provider for now

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,8 +1,9 @@
 class packetbeat::package {
 
   package { $packetbeat::package:
-    ensure  => $packetbeat::version,
-    source  => "/root/$packetbeat::package",
+    ensure   => $packetbeat::version,
+    provider => 'rpm',
+    source   => "/root/$packetbeat::package",
   }
 
   package { $packetbeat::params::libpcap:
