b35cdbb44405e81dc9e64f01ec93e234b4101a14
Jason Cochard <jasonc@uber.com>
2017-05-19 15:54:47 -0700

    add package to vm for local testing

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,6 +2,7 @@ class packetbeat::package {
 
   package { $packetbeat::package:
     ensure  => $packetbeat::version,
+    source  => "/root/$packetbeat::package",
   }
 
   package { $packetbeat::params::libpcap:
