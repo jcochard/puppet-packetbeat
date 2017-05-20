c6e6035f7194b4eaf84a725583c3f7f2934ed5a4
Jason Cochard <jasonc@uber.com>
2017-05-19 20:53:31 -0700

    syntax fix

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,7 +6,7 @@ class packetbeat::config {
   $es_password = $packetbeat::es_password
   $es_username = $packetbeat::es_username
 
-  notify{ "es_vip is: ${es_vip}"}
+  notify{ "es_vip is: ${es_vip}": }
 
   file { $packetbeat::config:
     ensure  => present,
