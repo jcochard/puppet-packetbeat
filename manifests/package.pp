d272edb447bb9dfb34e4fb34f328538e79df81f9
Jason Cochard <jasonc@uber.com>
2017-05-19 17:21:06 -0700

    use our internal repo

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,8 +2,6 @@ class packetbeat::package {
 
   package { $packetbeat::package:
     ensure   => 'installed',
-    provider => 'rpm',
-    source   => "/root/$packetbeat::package",
   }
 
   package { $packetbeat::params::libpcap:
